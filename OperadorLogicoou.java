51318bdeccacd96952b36033077a00f595d219d5
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-05 15:28:29 -0300

    Commit Inicial

---
+++ OperadorLogicoou.java
@@ -0,0 +1,18 @@
+/**
+Classe utilizada para demonstrar o uso do operador Lógico OU ( || )
+**/
+
+public class OperadorLogicoou {
+	public static void main (String [] args) {
+		boolean a = true;
+		boolean b = false;
+		boolean c = false;
+		
+		System.out.println(a || b);
+		System.out.println(b || c);
+	}
+}
+
+/**
+O operador ||, é chamado de “operador OU”. Ele retornará true se ao menos um dos dois operadores lógicos forem verdadeiros, logo, temos <operador1> || <operador2>.
+**/
